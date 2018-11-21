1ae4de85d377f58a757306d8edfd8932812e8dd6
Alfonso Emilio Gomez Selva <stein.hakase.vs@gmail.com>
2018-11-21 15:59:42 -0600

    added jwt token utils

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -1,12 +1,16 @@
-import { ApolloError } from 'apollo-server-express'
+import { ApolloError, AuthenticationError } from 'apollo-server-express'
 import utils from '../utils'
 
-const { pwHash } = utils
+const { pwHash, pwAuth, jwtRegister } = utils
 
 export default {
   Query: {
-    user: (root, args, context, info) => {
-      return 'user'
+    user: (root, { id }, { User }, info) => {
+      try {
+        return User.findById(id).exec()
+      } catch (e) {
+        throw new ApolloError(`Unable to find a user witht the provided ID`)
+      }
     }
   },
   Mutation: {
@@ -16,9 +20,21 @@ export default {
         let newUser = new User({ email, name, lastName, password })
         return newUser.save()
       } catch (e) {
-        console.log(e)
-        let error = new ApolloError(`Error creating new user ${e.message}`)
-        return error
+        throw new ApolloError(`Error creating new user ${e.message}`)
+      }
+    },
+    login: async (root, { email, password }, { User }, info) => {
+      try {
+        let user = await User.findOne({ email }).exec()
+        if (!user) { throw new Error('Invalid email or password') }
+
+        let validPw = await pwAuth(password, user.password)
+        if (!validPw) { throw new Error('Invalid email or password') }
+
+        let token = await jwtRegister({ id: user['_id'] })
+        return token
+      } catch (e) {
+        throw new AuthenticationError(`Error while Login in ${e.message}`)
       }
     }
   }
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,5 +1,11 @@
 import pwHash from './pwHash'
+import pwAuth from './pwAuth'
+import jwtRegister from './jwtRegister'
+import jwtAuth from './jwtAuth'
 
 export default {
-  pwHash
+  pwHash,
+  pwAuth,
+  jwtRegister,
+  jwtAuth
 }
